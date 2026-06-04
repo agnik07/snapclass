776f96b912bb911abe41ead6ad7319679f9ae68d
agnik <agnikdutta88@gmail.com>
2026-06-04 22:05:44 +0530

    Initial/updates

--- src/components/footer.py
+++ src/components/footer.py
@@ -9,5 +9,16 @@ def footer_home():
         </div>
 
                 """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+    # logo_url = ""
+    st.markdown(f"""
+        <div style='display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px; color: #181818'>
+            <p style="font-weight: bold"> Built by Agnik • SnapClass © 2026 </p>
+        </div>
+
+                """, unsafe_allow_html=True)
     
    
